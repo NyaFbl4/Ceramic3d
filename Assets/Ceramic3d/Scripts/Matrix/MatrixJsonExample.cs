06cd871c38a2ccfc64bb235dc37ab80f9b6e963f
NyaFbl4 <nikitaizmaylov1402@gmail.com>
2025-04-04 16:02:22 +0300

    Начал делать перенос матрицы в Json

--- Assets/Ceramic3d/Scripts/Matrix/MatrixJsonExample.cs
+++ Assets/Ceramic3d/Scripts/Matrix/MatrixJsonExample.cs
@@ -1,7 +1,16 @@
-﻿namespace Ceramic3d
+﻿using UnityEngine;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Ceramic3d
 {
     public class MatrixJsonExample
     {
-        
+        private List<Matrix4x4> _matrix = new();
+
+        public void SaveMatrixToJson()
+        {
+            //MatrixJso
+        }
     }
 }
